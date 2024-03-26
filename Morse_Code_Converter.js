6f8b591ae1182d54c27aa25aac785e6eb3d4b664
TrishalShrikant <trishalshrikant@gmail.com>
2024-03-26 21:25:33 +0530

    The final

---
+++ Morse_Code_Converter.js
@@ -0,0 +1,23 @@
+
+const morseCode = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
+    ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+  };
+  
+  function convertToMorse(text) {
+    let code=text.toUpperCase().split("");
+    let finalcode=code.map(char => morseCode[char] || char).join(' ');
+    return finalcode;
+}
+  
+  function handleConversion() {
+    const inputText = document.getElementById('inputText').value.trim();
+    document.getElementById('outputText').value = convertToMorse(inputText);
+  }
+  
+  document.getElementById('convertBtn').addEventListener('click', handleConversion);
+  
